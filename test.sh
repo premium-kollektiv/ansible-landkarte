45366e436b8483aad19c97d7afe8c7633ead4779
Christoph Haefner <christoph.haefner@mailbox.org>
2021-08-01 18:34:30 +0200

    Extend mariadb role to set and restrict root user password

--- test.sh
+++ test.sh
@@ -3,4 +3,7 @@
 set -x
 set -e
 
+# Make sure all needed ansible plugins are available
+ansible-galaxy collection install community.mysql
+
 ansible-playbook --check --diff --vault-password-file vault-password-file -i hosts main.yml
